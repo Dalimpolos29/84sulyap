9ee97bb0d313c81100c20dc0ea9ab020cdbd8587
Your Name <your.email@example.com>
2025-03-25 23:42:02 +0800

    fix: simplify SVG handling and update logo references

--- next.config.js
+++ next.config.js
@@ -10,28 +10,13 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.('.svg')
-    )
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: /url/ },
-        use: ['@svgr/webpack'],
-      }
-    )
-    fileLoaderRule.exclude = /\.svg$/i
-
-    return config
-  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+    return config;
+  }
 }
 
 module.exports = nextConfig 
